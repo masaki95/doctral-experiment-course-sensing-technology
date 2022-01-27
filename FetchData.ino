5c801f85f62ffe55c7695abbc6bde09d871b4596
masaki95 <49513670+masaki95@users.noreply.github.com>
2022-01-27 22:49:31 +0900

    Update FetchData.ino

--- FetchData.ino
+++ FetchData.ino
@@ -1,11 +1,5 @@
-#include <math.h>
-const int speakerPIN = 3;
-
 void setup() {
  Serial.begin(9600);
- pinMode(speakerPIN, OUTPUT);
- TCCR2A = _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
- TCCR2B = _BV(CS20);
 }
 
 void loop() {
